51960befe3467b6c3eaea048f2d4d6cb6b38fed8
sasaplus1 <sasaplus1@gmail.com>
2018-03-15 19:26:03 +0900

    added nuxt-blog config

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,5 +2,12 @@ module.exports = {
   generate: {
     dir: 'public',
   },
+  modules: [
+    [
+      'nuxt-blog', {
+        dirname: `${__dirname}/client/posts`,
+      },
+    ],
+  ],
   srcDir: 'client',
 };
